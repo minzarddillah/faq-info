3b99d96c15f0dbf669af2d0dbcb026a6d493995c
Minzard <minzard@myboost.id>
2019-01-16 14:31:02 +0700

    remove header

--- src/components/RedeemVoucher/RedeemVoucher.screen.js
+++ src/components/RedeemVoucher/RedeemVoucher.screen.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-// import HeaderBoostSpoot from '../../modules/HeaderBoostSpoot/HeaderBoostSpoot.container';
 
 export class RedeemVoucher extends Component {
-  // static navigationOptions = (props) => {
-  //   return {
-  //     header: <HeaderBoostSpoot {...props} title="Reward"/>
-  //   };
-  // };
   render() {
     return (
       <View>
